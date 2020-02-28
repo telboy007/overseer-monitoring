3d9d439691346915cfa00df0effb3f4c80ea0329
Terry Bonds <terry@hostmaker.co>
2020-02-28 23:05:58 +0000

    fix typos

--- deploy-serverless.sh
+++ deploy-serverless.sh
@@ -1,3 +1,36 @@
+#! /bin/bash
+
+# check serverless is installed before starting the fun
+SERVERLESS=`which serverless`
+
+if [ $? -ne 0 ]; then
+  echo "Serverless is not installed; exiting"
+  exit 1
+else
+  echo "Serverless found at $SERVERLESS"
+  serverless --version
+fi
+
+# check aws cli is installed before starting the fun
+AWS_CLI=`which aws`
+
+if [ $? -ne 0 ]; then
+  echo "AWS CLI is not installed; exiting"
+  exit 1
+else
+  echo "AWS CLI found at $AWS_CLI"
+  aws --version
+fi
+
+# check we've got an aws profile name
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <aws_profile_name>"
+  echo "Where:"
+  echo "   <aws_profile_name> = Profile to use when deploying code."
+  exit 2
+fi
+
+# add / update the folder locations below
 cd lambdas/master_controller
 serverless deploy --aws-profile profile-name
 cd ../certificates
